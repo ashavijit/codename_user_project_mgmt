dd62a942dd65a84036254dcb2efc5709deb86b2b
ashavijit <as2440@it.jgec.ac.in>
2023-06-28 10:51:24 +0530

    fixed

--- app.js
+++ app.js
@@ -2,34 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const { GraphQLClient, gql } = require('graphql-request');
-const URL = 'http://localhost:8080/graphql';
+const URL = 'http://localhost:8080/query';
 const graphQLClient = new GraphQLClient(URL, {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
     },
 });
-const query = gql`
-  query {
-    project(input: { id: "649a75623070691b350290c3" }) {
-      _id
-      ownerId
-      name
-      description
-      status
-    }
-  }
-`;
-JSON.stringify(query);
+
 
 app.get('/', async (req, res) => {
-    try {
-        const data = await graphQLClient.request(query);
-        res.send(data);
-    } catch (error) {
-        console.error('GraphQL Error:', error.response.errors);
-        res.status(500).send('Internal Server Error');
+    const ID = req.query.id;
+    const query = gql`    
+
+    query {
+        project(input: { id: "${ID}" }) {
+            _id
+            ownerId
+            name
+            description
+            status
+        }
     }
+    `;
+    const data = await graphQLClient.request(query);
+    res.send(data);
+ 
 });
 
 
